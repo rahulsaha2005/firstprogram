5bb9fb2278cc5fd80867b76a73bd5aecae5a6ef0
rahulsaha2005 <148579321+rahulsaha2005@users.noreply.github.com>
2024-07-29 20:20:37 +0000

    container config done

---
+++ 01_basic/constandvariable.js
@@ -0,0 +1,15 @@
+const accountId =144553;
+let accountEmail = "jived1224@gmail.com";
+let accountPassword = "rahulsaha@2005";
+accountCity ="faridabad";
+let accountState;
+
+// accountId=2 not allowed
+
+
+console.log(accountId);
+/*  
+prefer not to use var 
+because of issue 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
